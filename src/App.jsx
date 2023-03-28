09a1de39e8ab6857d7281b7c3af85cae83088395
Aditya <imaditya05@gmail.com>
2023-03-28 11:47:35 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Sidebar from './components/Sidebar';
+
+const App = () => {
+  return (
+    <div class="app">
+      <div class="container">
+        <Sidebar />
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Sidebar.css';
+
+const Sidebar = () => {
+  return (
+    <div class="sidebar">
+      {/* Logo */}
+      <div class="logo">dashr</div>
+      {/* menu */}
+      <div class="menu"></div>
+    </div>
+  );
+};
+
+export default Sidebar;
